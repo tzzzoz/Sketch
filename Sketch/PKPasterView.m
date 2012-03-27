58e88530752a12ffdb2a4ae63ef759ab226d28cf
tzzzoz <tzzzoz@gmail.com>
2012-03-27 16:12:06 +0800

    created PKPasterView, but not finished

---
+++ Sketch/PKPasterView.m
@@ -0,0 +1,13 @@
+//
+//  PKPasterView.m
+//  Sketch
+//
+//  Created by 付 乙荷 on 12-3-27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "PKPasterView.h"
+
+@implementation PKPasterView:UIView
+
+@end
