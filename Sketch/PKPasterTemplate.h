3f8b0126c3ac489acbccd4bec05fe00de38d87f9
tzzzoz <tzzzoz@gmail.com>
2012-03-29 00:28:18 +0800

    fixing conflicts

--- Sketch/PKPasterTemplate.h
+++ Sketch/PKPasterTemplate.h
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-//
-//  PKPasterTemplate.h
-//  Sketch
-//
-//  Created by 付 乙荷 on 12-3-27.
-//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-@interface PKPasterTemplate : NSObject {
-    NSMutableArray *geoPasterTemplates;
-    UIView *pasterView;
-}
-
-@end
-=======
 //
 //  PKPasterTemplate.h
 //  Sketch
@@ -31,4 +13,3 @@
     UIView *pasterView;
 }
 @end
->>>>>>> a6727d8d6e270b4dc4eaf6e184b2a6692d5c4970
