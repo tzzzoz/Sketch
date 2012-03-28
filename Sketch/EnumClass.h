5154bd580af59521b5c7defb988b88520c2c0bc3
Ray <raychen009@gmail.com>
2012-03-28 15:10:26 +0800

    PKGeometryTemplate,PKGeometryPaster加入了一些属性

---
+++ Sketch/EnumClass.h
@@ -0,0 +1,14 @@
+//
+//  EnumClass.h
+//  Sketch
+//
+//  Created by kwan terry on 12-3-27.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Sketch_EnumClass_h
+#define Sketch_EnumClass_h
+
+
+
+#endif
